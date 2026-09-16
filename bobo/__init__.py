4ce5f76e84d9ac06d621d6f9d561688fba1d90e8
condeDeveloper <183445812+condeDeveloper@users.noreply.github.com>
2026-09-16 10:16:01 -0300

    Pacote bobo

---
+++ bobo/__init__.py
@@ -0,0 +1,7 @@
+"""Bobo: um chatbot deliberadamente idiota, mas bem construído."""
+
+from bobo.cerebro import Cerebro
+from bobo.memoria import Memoria
+
+__all__ = ["Cerebro", "Memoria"]
+__version__ = "1.0.0"
